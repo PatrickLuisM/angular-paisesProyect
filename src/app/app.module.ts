70ec02194b0b9ab1f71d1e5f040b760b5ae3b54f
Patrick Luis <patricklmo32@gmail.com>
2021-09-07 19:32:29 -0500

    creando service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from "@angular/common/http";
 
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { PaisModule } from './pais/pais.module';
 import { SharedModule } from './shared/shared.module';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +16,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     PaisModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   bootstrap: [AppComponent]
 })
